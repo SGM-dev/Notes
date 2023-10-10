8a75a38e3ef31b8fcf4cb1f1da8e476ea6c1111e
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-10-10 06:01:03 +0000

    refactor updateNote for firestore

--- App.jsx
+++ App.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Editor from "./components/Editor";
 import Split from "react-split";
-import { nanoid } from "nanoid";
-import { onSnapshot, addDoc, doc, deleteDoc } from "firebase/firestore";
+import { onSnapshot, addDoc, doc, deleteDoc, setDoc } from "firebase/firestore";
 import { notesCollection, db } from "./firebase-config";
 export default function App() {
   const [notes, setNotes] = React.useState([]);
-
   const [currentNoteId, setCurrentNoteId] = React.useState("");
 
   const currentNote =
@@ -15,7 +13,6 @@ export default function App() {
 
   React.useEffect(() => {
     const unsubscribe = onSnapshot(notesCollection, function (snapshot) {
-      // Sync up our local notes array with the snapshot data
       const notesArr = snapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
@@ -39,20 +36,9 @@ export default function App() {
     setCurrentNoteId(newNoteRef.id);
   }
 
-  function updateNote(text) {
-    setNotes((oldNotes) => {
-      const newArray = [];
-      for (let i = 0; i < oldNotes.length; i++) {
-        const oldNote = oldNotes[i];
-        if (oldNote.id === currentNoteId) {
-          // Put the most recently-modified note at the top
-          newArray.unshift({ ...oldNote, body: text });
-        } else {
-          newArray.push(oldNote);
-        }
-      }
-      return newArray;
-    });
+  async function updateNote(text) {
+    const docRef = doc(db, "notes", currentNoteId);
+    await setDoc(docRef, { body: text }, { merge: true });
   }
 
   async function deleteNote(noteId) {

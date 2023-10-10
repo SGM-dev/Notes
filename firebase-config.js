8c33db311fd1713fdf5c39191bbb1c98972c0a71
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-10-10 03:28:11 +0000

    Setup firebase

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDelYbtpQ4XbLoelmuq3AE8oUL3qfHb3lc",
+  authDomain: "react-notes-cfcb2.firebaseapp.com",
+  projectId: "react-notes-cfcb2",
+  storageBucket: "react-notes-cfcb2.appspot.com",
+  messagingSenderId: "156751844423",
+  appId: "1:156751844423:web:8acf64c8ed2acbccdb8d62",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const notesCollection = collection(db, "notes");
